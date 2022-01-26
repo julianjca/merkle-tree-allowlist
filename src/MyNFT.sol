b53131b53ccddc5069b6940bfa19e5852736cdd5
Julian Anderson <juliancanderson@gmail.com>
2022-01-26 15:54:43 +0700

    chore: using OZ contract

--- src/MyNFT.sol
+++ src/MyNFT.sol
@@ -3,14 +3,15 @@ pragma solidity 0.8.11;
 
 import {ERC721} from "@solmate/tokens/ERC721.sol";
 import {MerkleProof} from "@openzeppelin/utils/cryptography/MerkleProof.sol";
-import "./utils/Strings.sol";
+import {Strings} from "@openzeppelin/utils/Strings.sol";
 
 error WrongEtherAmount();
 error TokenDoesNotExist();
 error InvalidProof();
+error MaxSupplyReached();
 
-/// @title Greeter
-/// @author Andreas Bigger <andreas@nascent.xyz>
+/// @title MyNFT
+/// @author Julian <juliancanderson@gmail.com>
 contract MyNFT is ERC721 {
     using Strings for uint256;
 
@@ -18,6 +19,7 @@ contract MyNFT is ERC721 {
     uint256 public immutable maxSupply = 10000;
 
     string public baseURI;
+    uint256 public totalSupply;
 
     bytes32 public merkleRoot =
         0x3fcf3077fd0856bf7cdce49a94e3e391b16cb1707afd9d03e17232b19977b7ad;
@@ -33,13 +35,16 @@ contract MyNFT is ERC721 {
     }
 
     function mintWhitelist(bytes32[] calldata merkleProof) external payable {
-        if (msg.value != price) {
-            revert WrongEtherAmount();
-        }
+        if (msg.value != price) revert WrongEtherAmount();
+
+        if (totalSupply + 1 > maxSupply) revert MaxSupplyReached();
+
         bytes32 leaf = keccak256(abi.encodePacked(msg.sender));
         if (verifyMerkle(merkleProof, leaf)) {
             revert InvalidProof();
         }
+
+        _mint(msg.sender, totalSupply++);
     }
 
     function verifyMerkle(bytes32[] calldata proof, bytes32 leaf)
