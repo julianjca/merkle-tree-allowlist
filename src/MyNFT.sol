18c9b15753f53c89536e3becdf1431e691c50693
Julian Anderson <juliancanderson@gmail.com>
2022-01-26 14:35:59 +0700

    feat: creating MyNFT contract

--- src/MyNFT.sol
+++ src/MyNFT.sol
@@ -2,17 +2,53 @@
 pragma solidity 0.8.11;
 
 import "@solmate/tokens/ERC721.sol";
+import "./utils/Strings.sol";
+
+error WrongEtherAmount();
+error TokenDoesNotExist();
 
 /// @title Greeter
 /// @author Andreas Bigger <andreas@nascent.xyz>
 contract MyNFT is ERC721 {
-    // put the merkle root + 0x from the merkleTree.js script here
+    using Strings for uint256;
+
+    uint256 public immutable price = 0.1 ether;
+    uint256 public immutable maxSupply = 10000;
+
+    string public baseURI;
+
     bytes32 public merkleRoot =
-        0xa527e0dc1cfa5d86e65d1e30cecb1d985cdb5caf9792ca83df2b01a326b83edf;
+        0x3fcf3077fd0856bf7cdce49a94e3e391b16cb1707afd9d03e17232b19977b7ad;
 
-    constructor(string memory _name, string memory _symbol)
-        ERC721(name, symbol)
-    {}
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        bytes32 _merkleRoot,
+        string memory _baseURI
+    ) ERC721(_name, _symbol) {
+        merkleRoot = _merkleRoot;
+        baseURI = _baseURI;
+    }
 
-    function mintWhitelist() external {}
+    function mintWhitelist(bytes32[] calldata merkleProof) external payable {
+        if (msg.value != price) {
+            revert WrongEtherAmount();
+        }
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        if (ownerOf[tokenId] == address(0)) {
+            revert TokenDoesNotExist();
+        }
+
+        return
+            bytes(baseURI).length > 0
+                ? string(abi.encodePacked(baseURI, tokenId.toString(), ".json"))
+                : "";
+    }
 }
